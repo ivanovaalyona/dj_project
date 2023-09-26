6d11fa7ed4900c21334e698398c59c64cbe5b1de
Alyona_Ivanova <alenaalyakseeyna@gmail.com>
2023-09-26 12:49:22 +0300

    add docker

--- CarshowroomProject/config/settings.py
+++ CarshowroomProject/config/settings.py
@@ -80,10 +80,14 @@ WSGI_APPLICATION = "config.wsgi.application"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
+    'default': {
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+            'NAME': os.environ.get('POSTGRES_DB'),
+            'USER': os.environ.get('POSTGRES_USER'),
+            'PASSWORD': os.environ.get('POSTGRES_PASSWORD'),
+            'HOST': 'postgres',
+            'PORT': '5432',
+        }
 }
 
 
